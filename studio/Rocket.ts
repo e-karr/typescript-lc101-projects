d07b812f05002957857fd0b93c37cf37266b7dfa
Elizabeth Karr <e.karr93@icloud.com>
2023-04-25 13:50:59 -0500

    declare Rocket class properties

--- studio/Rocket.ts
+++ studio/Rocket.ts
@@ -1,3 +1,9 @@
+import { Astronaut } from './Astronaut';
+import { Cargo } from './Cargo';
+
 export class Rocket {
-    
+    name: string;
+    totalCapacityKg: number;
+    cargoItems: Cargo[] = [];
+    astronauts: Astronaut[] = [];
 }
